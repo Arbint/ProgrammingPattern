a044204728f609deddfff9dc3831f067cbe89646
Li <jili1@uiwtx.edu>
2023-09-05 15:26:25 -0500

    added button

--- ProgrammingPattern/MainMenuState.cs
+++ ProgrammingPattern/MainMenuState.cs
@@ -10,11 +10,19 @@ namespace ProgrammingPattern
             mFont = new Font("BAUHS93.TTF");
             mTitleText = new Text("Snake Game", mFont);
             mTitleText.Position = new Vector2f(200f, 200f);
-
+            mButtonText = new Text("Start Game", mFont);
+            
+            mButton = new RectangleShape();
+            mButton.Size = new Vector2f(200f, 80f);
+            mButton.Position = mTitleText.Position + new Vector2f(0, 50f);
+            mButton.FillColor = new Color(128, 130, 128);
+            mButtonText.Position = mButton.Position + new Vector2f(30f, 15f);
         }
         public override void Render(RenderWindow window)
         {
             window.Draw(mTitleText);
+            window.Draw(mButton);
+            window.Draw(mButtonText);
         }
 
         public override void Update(float deltaTime)
@@ -24,6 +32,9 @@ namespace ProgrammingPattern
 
         Font mFont;
         Text mTitleText;
+
+        Text mButtonText;
+        RectangleShape mButton;
 
     }
 }
